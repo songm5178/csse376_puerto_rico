e75229c4402696e15d4d4ad485cb16698e580dbc
IsaiahSmith <smithij@rose-hulman.edu>
2015-03-26 23:46:22 -0400

    mayor test fails
    of course...

--- src/csse376_puerto_rico/Tests.java
+++ src/csse376_puerto_rico/Tests.java
@@ -7,9 +7,14 @@ import org.junit.Test;
 public class Tests {
 	
 	@Test
-	public void test_playersRole() {
+	public void test_playersRoleAsTrader() {
 		Player player = new Player("trader");
 		assertEquals("trader",player.getRole());
+	}
+	@Test
+	public void test_playersRoleAsMayor() {
+		Player player = new Player("mayor");
+		assertEquals("mayor",player.getRole());
 	}
 
 }
