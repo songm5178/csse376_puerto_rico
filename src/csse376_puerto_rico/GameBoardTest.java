a774d8946ef31b9d8391982ebac6bc613bc04741
songm <songm@rose-hulman.edu>
2015-04-02 20:51:12 -0400

    Test for GameBoardTest

---
+++ src/csse376_puerto_rico/GameBoardTest.java
@@ -0,0 +1,20 @@
+package csse376_puerto_rico;
+
+import javax.swing.JFrame;
+
+import org.junit.Test;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author songm.
+ *         Created Apr 2, 2015.
+ */
+public class GameBoardTest {
+
+	@Test
+	public void initial_test() {
+		GameBoardGUI gb = new GameBoardGUI(new JFrame());
+	}
+
+}
