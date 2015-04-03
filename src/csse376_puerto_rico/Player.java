244b456b319926013bcb5f539b012c40cdb583ad
songm <songm@rose-hulman.edu>
2015-04-02 21:07:50 -0400

    Fixed getRolesList in PlayerTest

--- src/csse376_puerto_rico/Player.java
+++ src/csse376_puerto_rico/Player.java
@@ -1,19 +1,35 @@
 package csse376_puerto_rico;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
-	
+
 	private String role;
 	private boolean isTurn;
 	private boolean isMayor;
+	private static List<String> roles = new ArrayList<String>(Arrays.asList("Settler",
+			"Mayor", "Builder", "Craftsman", "Trader", "Captain", "Prospector"));
 
-	public Player(String role){
+	public Player(String role) {
 		this.role = role;
 		this.isMayor = false;
 		this.isTurn = false;
 	}
-	
-	public String getRole(){
+
+	public String getRole() {
 		return this.role;
+	}
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @return
+	 */
+	public static List<String> getRoles() {
+
+		return roles;
 	}
 
 }
