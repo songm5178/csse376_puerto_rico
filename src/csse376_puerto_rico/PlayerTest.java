0eeb868ff0cea37ba15e37d58a7ac8b32813d3c4
songm <songm@rose-hulman.edu>
2015-04-02 21:02:12 -0400

    Added getRolesList in PlayerTest

--- src/csse376_puerto_rico/PlayerTest.java
+++ src/csse376_puerto_rico/PlayerTest.java
@@ -2,6 +2,9 @@ package csse376_puerto_rico;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class PlayerTest {
@@ -20,6 +23,20 @@ public class PlayerTest {
 	public void test_playersRoleAsBuilder() {
 		Player player = new Player("builder");
 		assertEquals("builder",player.getRole());
+	}
+	
+	@Test
+	public void test_getRolesList() {
+		List<String> roles = new ArrayList<String>();
+		roles.add("Settler");
+		roles.add("Mayor");
+		roles.add("Builder");
+		roles.add("Craftsman");
+		roles.add("Trader");
+		roles.add("Captain");
+		roles.add("Prospector");
+		
+		assertEquals(roles, Player.getRoles());
 	}
 	
 	
