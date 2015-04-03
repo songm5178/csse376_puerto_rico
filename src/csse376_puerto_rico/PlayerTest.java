46d2a217897b66e94b4222021a4ef82c99203c98
songm <songm@rose-hulman.edu>
2015-04-02 21:08:03 -0400

    changed formatting

--- src/csse376_puerto_rico/PlayerTest.java
+++ src/csse376_puerto_rico/PlayerTest.java
@@ -8,23 +8,25 @@ import java.util.List;
 import org.junit.Test;
 
 public class PlayerTest {
-	
+
 	@Test
 	public void test_playersRoleAsTrader() {
 		Player player = new Player("trader");
-		assertEquals("trader",player.getRole());
+		assertEquals("trader", player.getRole());
 	}
+
 	@Test
 	public void test_playersRoleAsMayor() {
 		Player player = new Player("mayor");
-		assertEquals("mayor",player.getRole());
+		assertEquals("mayor", player.getRole());
 	}
+
 	@Test
 	public void test_playersRoleAsBuilder() {
 		Player player = new Player("builder");
-		assertEquals("builder",player.getRole());
+		assertEquals("builder", player.getRole());
 	}
-	
+
 	@Test
 	public void test_getRolesList() {
 		List<String> roles = new ArrayList<String>();
@@ -35,10 +37,8 @@ public class PlayerTest {
 		roles.add("Trader");
 		roles.add("Captain");
 		roles.add("Prospector");
-		
+
 		assertEquals(roles, Player.getRoles());
 	}
-	
-	
 
 }
