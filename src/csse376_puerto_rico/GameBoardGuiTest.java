0690e6fc19f5096194aa2b2e69939bbfc3bfc0e1
songm <songm@rose-hulman.edu>
2015-04-16 19:17:35 -0400

    Added class for testing

---
+++ src/csse376_puerto_rico/GameBoardGuiTest.java
@@ -0,0 +1,20 @@
+package csse376_puerto_rico;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author songm.
+ *         Created Apr 16, 2015.
+ */
+public class GameBoardGuiTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
