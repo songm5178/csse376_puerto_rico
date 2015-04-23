62aab8688576fe2246f4a705debba753c1375c0e
songm <songm@rose-hulman.edu>
2015-04-23 00:09:25 -0400

    Fixed buildings list test

--- src/csse376_puerto_rico/Buildings.java
+++ src/csse376_puerto_rico/Buildings.java
@@ -1,6 +1,39 @@
 package csse376_puerto_rico;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Buildings {
-	public static String small_indigo_plant = "Small Indigo Plant";
+
+	private ArrayList<Building> buildings;
+
+	public Buildings() {
+		buildings = new ArrayList<Building>();
+
+		
+		List<String> buildingNames = Arrays.asList("Small Indigo Plant",
+				"Small Sugar Mill", "Small Market", "Hacienda",
+				"ConstructionHut", "Small Warehouse", "Indigo Plant",
+				"Surgar Mill", "Hospice"); //TODO: add more
+		
+		for(String name: buildingNames){
+			buildings.add(new Building(name));
+		}
+
+	}
+
+	public ArrayList<Building> getBuildings() {
+
+		return buildings;
+	}
+
+	public class Building {
+
+		public String name;
+
+		public Building(String name) {
+
+		}
+	}
 }
