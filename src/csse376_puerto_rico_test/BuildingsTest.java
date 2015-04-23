a17238896fe6915ed2cd9fef79a371b42339b532
songm <songm@rose-hulman.edu>
2015-04-22 23:48:46 -0400

    Added Buildings Test basic Test

---
+++ src/csse376_puerto_rico_test/BuildingsTest.java
@@ -0,0 +1,22 @@
+package csse376_puerto_rico_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import csse376_puerto_rico.Buildings;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author songm.
+ *         Created Apr 22, 2015.
+ */
+public class BuildingsTest {
+
+	@Test
+	public void basic_test() {
+		Buildings b = new Buildings();
+	}
+
+}
