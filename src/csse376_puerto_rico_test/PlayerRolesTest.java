7174fed62aa528dd5d9c5904e5b311b46322e765
songm <songm@rose-hulman.edu>
2015-04-22 21:48:03 -0400

    Added PlayerRoles Enum Test to clean up players class

---
+++ src/csse376_puerto_rico_test/PlayerRolesTest.java
@@ -0,0 +1,18 @@
+package csse376_puerto_rico_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import csse376_puerto_rico.PlayerRoles;
+
+
+public class PlayerRolesTest {
+
+	@Test
+	public void basic_test() {
+		
+		PlayerRoles.values();
+	}
+
+}
