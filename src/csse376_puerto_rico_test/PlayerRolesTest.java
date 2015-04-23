057095ae499c4ac6c3d3f01b16acc8f82773f166
songm <songm@rose-hulman.edu>
2015-04-22 21:57:11 -0400

    Added final test for list of all settlers

--- src/csse376_puerto_rico_test/PlayerRolesTest.java
+++ src/csse376_puerto_rico_test/PlayerRolesTest.java
@@ -2,32 +2,47 @@ package csse376_puerto_rico_test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import csse376_puerto_rico.PlayerRoles;
-
 
 public class PlayerRolesTest {
 
 	@Test
 	public void basic_test() {
-		
+
 		PlayerRoles.values();
 	}
+
 	@Test
 	public void captain_test() {
-		
-		assertEquals(PlayerRoles.Captain, "Captain");
+
+		assertEquals("Captain", PlayerRoles.Captain.toString());
 	}
+
 	@Test
 	public void settler_test() {
-		
-		assertEquals(PlayerRoles.Settler, "Settler");
+
+		assertEquals("Settler", PlayerRoles.Settler.toString());
 	}
+
 	@Test
 	public void builder_test() {
-		
-		assertEquals(PlayerRoles.Builder, "Builder");
+
+		assertEquals("Builder", PlayerRoles.Builder.toString());
+	}
+
+	@Test
+	public void roles_test() {
+		List<String> roles = new ArrayList<String>(Arrays.asList("Settler",
+				"Builder", "Craftsman", "Trader", "Captain", "Prospector",
+				"Mayor"));
+		PlayerRoles[] pRoles = PlayerRoles.values();
+		assertEquals(roles.toArray(),pRoles);
 	}
 
 }
