3d208759a1c355151a8b6a3ad6b53aced211a2c6
songm <songm@rose-hulman.edu>
2015-04-24 12:29:43 -0400

    Added All Tests class for codecover

---
+++ src/csse376_puerto_rico_test/AllTests.java
@@ -0,0 +1,13 @@
+package csse376_puerto_rico_test;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ BuildingsTest.class, GameBoardGuiTest.class,
+		GameBoardTest.class, PlayerRolesTest.class, PlayerTest.class })
+public class AllTests {
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("csse376_puerto_rico_test.AllTests");
+	}
+}
