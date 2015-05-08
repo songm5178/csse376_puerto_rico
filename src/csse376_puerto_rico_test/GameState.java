7b60a4bccd150d294a579662e01db0bb9917168d
songm <songm@rose-hulman.edu>
2015-05-07 21:26:32 -0400

    Revert "Added getGoods addGoods in GameState"
    This reverts commit 29047332b2057be3d797e2e3d78e8e0b133934ad.

--- src/csse376_puerto_rico_test/GameState.java
+++ src/csse376_puerto_rico_test/GameState.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import csse376_puerto_rico.Player;
-
 /**
  * TODO Put here a description of what this class does.
  *
@@ -17,19 +15,6 @@ public class GameState {
 	@Test
 	public void test() {
 		new GameState();
-	}
-	
-	@Test
-	public void testGoods(){
-		
-		GameState g = new GameState();
-		
-		int coffee = g.getGood(Player.Good.COFFEE);
-		assertEquals(0, coffee);
-		
-		g.addGood(Player.Good.COFFEE);
-		coffee = g.getGood(Player.Good.COFFEE);
-		assertEquals(1, coffee);
 	}
 
 }
