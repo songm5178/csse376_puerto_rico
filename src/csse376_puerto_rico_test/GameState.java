182db96a44757e55de5ed48d0ec6f62a82f0903c
songm <songm@rose-hulman.edu>
2015-05-07 21:24:13 -0400

    Added basic test for GameState

---
+++ src/csse376_puerto_rico_test/GameState.java
@@ -0,0 +1,20 @@
+package csse376_puerto_rico_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author songm.
+ *         Created May 7, 2015.
+ */
+public class GameState {
+
+	@Test
+	public void test() {
+		new GameState();
+	}
+
+}
